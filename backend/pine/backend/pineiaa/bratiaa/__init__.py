264a24926f89ea67bdbe1d61ba781e3908c83880
brantapl <34453873+brantapl@users.noreply.github.com>
2020-06-28 16:13:18 -0400

    Updating bratiaa copyright

--- backend/pine/backend/pineiaa/bratiaa/__init__.py
+++ backend/pine/backend/pineiaa/bratiaa/__init__.py
@@ -1,4 +1,4 @@
-# (C) 2019 The Johns Hopkins University Applied Physics Laboratory LLC.
+# Copyright (c) 2019 Tobias Kolditz
 
 from ..bratiaa.agree import compute_f1_agreement, iaa_report, AnnFile, F1Agreement, Document, input_generator
 from ..bratiaa.evaluation import exact_match_instance_evaluation, exact_match_token_evaluation, Annotation
